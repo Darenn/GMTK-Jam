8a6a3d23637bdf0f802bdc58935105963774d899
Pc-Guillaume07\Guillaume <guillaume.dufour07@gmail.com>
2018-09-01 22:14:30 +0200

    Guillaume commit

---
+++ GMTK-Jam/Assets/Scripts/Save_Scene_Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Save_Scene_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        PlayerPrefs.SetInt("Name_Scene", (Application.loadedLevelName));
+       
+        Debug.Log(PlayerPrefs.GetInt("Name_Scene").ToString);
+	}
+	
+}
