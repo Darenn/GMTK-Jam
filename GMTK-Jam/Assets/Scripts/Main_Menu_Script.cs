addcf5d8aace6ef25420d81ea84430031615467f
Pc-Guillaume07\Guillaume <guillaume.dufour07@gmail.com>
2018-09-01 21:09:44 +0200

    Menu_Guillaume

---
+++ GMTK-Jam/Assets/Scripts/Main_Menu_Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu_Script : MonoBehaviour {
+
+	public void NewGame()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
