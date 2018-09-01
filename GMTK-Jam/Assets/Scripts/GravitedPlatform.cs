ff55cf7a6ce041b19e65004219646e8ad96ebc30
Darenn Keller <keller.darenn@gmail.com>
2018-09-01 18:53:28 +0200

    First mechanic okay.

---
+++ GMTK-Jam/Assets/Scripts/GravitedPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitedPlatform : MonoBehaviour {
+
+    GameObject player;
+    Rigidbody2D player_rb;
+    Collider2D collider;
+
+    bool playerIsInRange = false;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        player_rb = player.GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerIsInRange)
+        {
+            Vector3 closestPoint = collider.bounds.ClosestPoint(player.transform.position);
+            player_rb.AddForce((closestPoint - player.transform.position).normalized * 100);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsInRange = true;
+
+            player.GetComponent<PlayerController>().CurrentPlatform = collider;
+        }
+    }
+
+    public void PlayerLeavesPlatform()
+    {
+        playerIsInRange = false;
+    }
+}
